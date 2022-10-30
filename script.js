307b4ce5ef60f03b3cc45c69d2c2e47b867b252b
abdelsurya <abdelsurya@gmail.com>
2022-10-30 16:33:38 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-$(".btn-toggle").click(function () {
-  $(".menu-content").toggle();
+$(".btn-toggle1").click(function () {
+  $(".menu-content-jq").toggle();
+});
+
+$(function () {
+  $("#tabs").tabs();
+});
+
+$(".clicktobig").click(function () {
+  $(".box").animate(
+    {
+      marginLeft: "651px",
+      height: "300px",
+      width: "600px",
+    },
+    1000
+  );
+});
+
+$(".clicktosmall").click(function () {
+  $(".box").animate(
+    {
+      marginLeft: "0px",
+      height: "150px",
+      width: "300px",
+    },
+    1000
+  );
+});
+
+$(".fadeto").click(function () {
+  $(".boxfade").fadeIn(3000);
+});
+
+$("#tabs li a:not(:first)").addClass("incative");
+$(".tab-content-custom").hide();
+$(".tab-content-custom:first").show();
+
+$("#tabs li a").click(function () {
+  var n = $(this).attr("id");
+  if ($(this).hasClass("inactive")) {
+    $("#tabs li a").addClass("inactive");
+    $(this).removeClass("inactive");
+
+    $(".tab-content-custom").hide();
+    $("#" + n + "content").fadeIn("slow");
+  }
 });
